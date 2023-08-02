b38f8e3b854ed9f93b30255f270929f55742f936
maryamebrahimzade <ma.ebrahimzadeh@gmail.com>
2023-08-02 11:12:00 +0330

    add->heapsort

---
+++ src/main/java/com/example/sort/heapsort/Main.java
@@ -0,0 +1,13 @@
+package com.example.sort.heapsort;
+
+public class Main {
+    public static void main(String[] args) {
+        int a[] = {45, 7, 20, 40, 25, 23, -2};
+        int n = a.length;
+        System.out.print("Before sorting array elements are - \n");
+        HeapSort.printArr(a, n);
+        HeapSort.heapSort(a, n);
+        System.out.print("\nAfter sorting array elements are - \n");
+        HeapSort.printArr(a, n);
+    }
+}
